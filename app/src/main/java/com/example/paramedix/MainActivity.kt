05f010b449ec6caa49c1ddaa5352b71779b2565c
ankitwest <anshkrrr@gmail.com>
2021-06-27 22:53:08 +0530

    a

--- app/src/main/java/com/example/paramedix/MainActivity.kt
+++ app/src/main/java/com/example/paramedix/MainActivity.kt
@@ -10,21 +10,27 @@ import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    
     lateinit var loginFragmentAdapter:LoginFragmentAdapter;
     private lateinit var viewPager: ViewPager2;
     private lateinit var tabLayout: TabLayout;
     private var page:Int=2
     val headText= arrayOf("Medical Worker","Beneficiary")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         supportActionBar!!.hide()
         window.decorView.systemUiVisibility=View.SYSTEM_UI_FLAG_FULLSCREEN
         setContentView(R.layout.activity_main)
+
         tabLayout=findViewById(R.id.tabsLayout)
         loginFragmentAdapter= LoginFragmentAdapter(supportFragmentManager,lifecycle)
+
         viewPager=findViewById(R.id.container)
         viewPager.setCurrentItem(page)
         viewPager.adapter=loginFragmentAdapter
+
         TabLayoutMediator(tabLayout,viewPager){tab,position->
             tab.text="Login as "+headText[position]
         }.attach()
